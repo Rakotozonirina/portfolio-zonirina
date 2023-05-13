ced192dfea744e6c0ef94e348f8c26cf91ab5a49
Zo Nirina <tomzonirina@gmail.com>
2023-05-13 19:36:42 +0300

    Adding the index Js

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+const Navbar = () => {
+return (
+    <>
+        <header>
+            
+        </header>
+    </>
+)
+}
+
+export default Navbar;
