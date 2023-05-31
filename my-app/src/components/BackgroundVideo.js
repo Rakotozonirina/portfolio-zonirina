30bcbbf818aadd2827808059c21c741c5f211eba
Zo Nirina <tomzonirina@gmail.com>
2023-05-31 11:32:48 +0300

    I am going to put the navigation bar

--- my-app/src/components/BackgroundVideo.js
+++ my-app/src/components/BackgroundVideo.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/BackgroundVideo.css';
 import Video from '../assets/video/my-fond-light.mp4';
+import Navbar from './Navbar';
 const BackgroundVideo = () => {
 return (
     <main>
+        <Navbar/>
         <video autoPlay loop muted>
             <source src={Video} type='video/mp4' />
         </video>
--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Logo from '../assets/photo-logo/my-logo.svg';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import '../styles/Navbar.css';
 
 const Navbar = () => {
 return (
     <>
         <header>
-            
+            <nav className='navbar navbar-expand-lg navbar-light back-color'>
+                <section className='container-fluid'>
+                    <a className='navbar-brand nav-link navbar-brand-img' href="#">
+                        <img src={Logo} alt='logo' width='100' height='50'/>
+                    </a>
+                </section>
+            </nav>
         </header>
     </>
 )
